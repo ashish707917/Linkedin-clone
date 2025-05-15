7592e7dd5ab27deb38ba175a8ea70f2b8af98d1c
ashish707917 <ashish707917@gmail.com>
2025-05-15 22:41:14 +0530

    krishna

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -8,7 +8,7 @@ const Comment = ({ comment }: { comment: ICommentDocument }) => {
     return (
         <div className='flex gap-2 my-4'>
             <div className='mt-2'>
-                <ProfilePhoto src={comment?.user?.ProfilePhoto!} />
+                <ProfilePhoto src={comment?.user?.ProfilePhoto ?? '/banner.jpg'} />
             </div>
             <div className='flex flex-1 justify-between p-3 bg-[#F2F2F2]'>
                 <div>
--- app/components/CommentInput.tsx
+++ app/components/CommentInput.tsx
@@ -22,8 +22,7 @@ const CommentInput = ({ postId }: { postId: string }) => {
     return (
         <form action={(formData)=> commentActionHandler(formData)}>
             <div className='flex items-center gap-2'>
-                <ProfilePhoto
-                 src={user?.imageUrl!} />
+                <ProfilePhoto src={user?.imageUrl ?? "/banner.jpg"} />
                 <Input
                     type="text"
                     name="inputText"
